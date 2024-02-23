8c01308e8cdde006e8c6278bb8f6bafd2edff64d
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-02-22 21:13:35 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var x = 0; // score
 var o = 0;
 num = 0
-var winner;
+var winner; //set winner as false
 let xWins = document.getElementById("xWins"), oWins = document.getElementById("oWins") // set score to variables
 function reset(){ // reset all cells with empty quotes
     document.getElementById("1").innerHTML=""
@@ -14,7 +14,8 @@ function reset(){ // reset all cells with empty quotes
     document.getElementById("7").innerHTML=""
     document.getElementById("8").innerHTML=""
     document.getElementById("9").innerHTML=""
-    num = 0
+    num = 0 // reset num back to 0
+    winner = false //when reset, make winner false.
 }
 // function called onclick
 function box(box){
@@ -36,21 +37,9 @@ function box(box){
         }
     }
 }
-// determine all indexes that are possible win conditions.
-// var wins = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-// ];
-//  put indexes into the temporary list options
-function checkwinner(){
-    var options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var wins = [
+function checkwinner(){ //func to check winner
+    var options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]; //calls index of every cell
+    var wins = [ //the cells that have to be the same for a win
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -60,14 +49,13 @@ function checkwinner(){
         [0, 4, 8],
         [2, 4, 6]
     ];
-    for(let item of wins){
-        let [a,b,c] = item;
+    for(var item of wins){ //loop through all possible win cons
+        let [a,b,c] = item; //make a var to equal the 3 cells in current index of wins
         const cellA = document.getElementById(options[a]).innerHTML; // set a variable to the first, second, and third of each win condition
-        const cellB = document.getElementById(options[b]).innerHTML; // set a variable to the
+        const cellB = document.getElementById(options[b]).innerHTML;
         const cellC = document.getElementById(options[c]).innerHTML;
-        // alert(cellA + " " + cellB + " " + cellC)
-        if (cellA || cellB || cellC){
-            if(cellA == cellB && cellB == cellC){ // if not empty, must be full, if all equal, there is winner.
+        if(cellA || cellB || cellC){ // if their are no empty cells.
+            if(cellA == cellB && cellB == cellC){ //if all cells in winning order are the same
                 winner = true; // set a winner
                 // alert("Winner: " + cellA)
                 break;
@@ -75,17 +63,22 @@ function checkwinner(){
         }
     }
     if(winner){ // if there is a winner
-        if(turn.innerHTML == "Turn: O"){ // if the turn is O, give x a point because the turn switches.
+        if(turn.innerHTML == "Turn: O"){ // if the turn is O, give x a point because the turn has switched.
             xWins.innerHTML = "&#x2715; | Wins : " + x++ ; // alert who won and add one point
-            winner = false;
+            winner = true;
             alert(`X has won!`);
         }else{
-            oWins.innerHTML = "&#9711; | Wins : " + o++ ;
-            winner = false;
+            oWins.innerHTML = "&#9711; | Wins : " + o++ ; // same process for o
+            winner = true;
             alert(`O has won!`);
         }
-    }else if(num == 9){
+    }else if(num == 9){ // if num == 9, all cells must have been used up with no winner.
         alert("Draw!") // if all cells are full but no 3 in a row, alert a draw
     }
 }
-
+if(winner){
+    const cellAC = cellA.innerHTML, cellBC = cellB.innerHTML, cellCC = cellC.innerHTML; // get content of every cell
+    cellAC.style.backgroundColor = 'red'; // Highlight the cells by changing their background color
+    cellBC.style.backgroundColor = 'red';
+    cellCC.style.backgroundColor = 'red';
+}
